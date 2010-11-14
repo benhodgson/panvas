0e145d57af4d2fbe8327056603b052c57e53b69b
Ben Hodgson <ben@benhodgson.com>
2010-11-14 14:24:04 +0000

    The canvas now rotates, with the simulation’s gravity rotation to oppose it.

--- panvas.js
+++ panvas.js
@@ -1,11 +1,16 @@
 (function(window, undefined) {
   
-  var g = -500, // px/sec^2
-      maxParticles = 100,
+  var g = -10, // gravity / px/sec^2
+      rotationPeriod = 20, // secs
       coefficientOfRestitution = 0.75,
-      colours = ['#900', '#090', '#009', '#099', '#909'];
+      maxInitialComponentVelocity = 1000,
+      maxParticles = 100,
+      colours = ['#900', '#090', '#009', '#099', '#909'],
+      sizes = [3, 5, 7];
   
   var document = window.document,
+      TWO_PI = Math.PI * 2, // rads
+      rotationFrequency = TWO_PI / rotationPeriod, // rads/sec
       ctx,
       canvas,
       canvasWidth,
@@ -14,72 +19,81 @@
       originY,
       lastSim,
       scene = [],
-      idx = 0;
+      idx = 0,
+      gx = 0,
+      gy = 0,
+      
+      // "up" is the direction in which to orient the canvas
+      // "down" is the direction in which gravity acts
+      thisWayUp = 0, // rads
+      thisWayDown = thisWayUp + Math.PI; // rads
   
   function Particle(radius) {
     this.radius = radius;
     this.fill = "#000";
     this.x = originX;
     this.y = originY;
-    this.dx = 0; // pixels per second
-    this.dy = 0; // pixels per second
-    this.bounces = 0;
-    this.simulate = true;
+    this.dx = 0; // px/sec
+    this.dy = 0; // px/sec
   };
   
   Particle.prototype.draw = function(ctx) {
     ctx.beginPath();
     ctx.fillStyle = this.fill;
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true); 
+    ctx.arc(this.x, this.y, this.radius, 0, TWO_PI, true); 
     ctx.closePath();
     ctx.fill();
   };
   
   Particle.prototype.advanceSimulationBy = function(interval) {
     
-    var intervalSecs = interval / 1000,
-        dg = g * intervalSecs,
-        dx = this.dx * intervalSecs,
-        dy = this.dy * intervalSecs;
+    this.dy += gy;
+    this.dx += gx;
+    this.x += this.dx * interval;
+    this.y += this.dy * interval;
     
-    if(this.simulate) {
-      this.dy -= dg;
-      this.x += dx;
-      this.y += dy;
-      
-      if(this.y + this.radius > canvasHeight) {
-          this.bounces++;
-          this.y = canvasHeight - this.radius;
-          this.dy = -1 * this.dy * coefficientOfRestitution;
-      } else if(this.y - this.radius < 0) {
-          this.bounces++;
-          this.y = 0 + this.radius;
-          this.dy = -1 * this.dy * coefficientOfRestitution;
-      }
-
-      if(this.x + this.radius > canvasWidth) {
-          this.bounces++;
-          this.x = canvasWidth - this.radius;
-          this.dx = -1 * this.dx * coefficientOfRestitution;
-      } else if(this.x - this.radius< 0) {
-          this.bounces++;
-          this.x = 0 + this.radius;
-          this.dx = -1 * this.dx * coefficientOfRestitution;
-      }
+    if(this.y + this.radius > canvasHeight) {
+        this.y = canvasHeight - this.radius;
+        this.dy = -1 * this.dy * coefficientOfRestitution;
+    } else if(this.y - this.radius < 0) {
+        this.y = 0 + this.radius;
+        this.dy = -1 * this.dy * coefficientOfRestitution;
+    }
+    
+    if(this.x + this.radius > canvasWidth) {
+        this.x = canvasWidth - this.radius;
+        this.dx = -1 * this.dx * coefficientOfRestitution;
+    } else if(this.x - this.radius< 0) {
+        this.x = 0 + this.radius;
+        this.dx = -1 * this.dx * coefficientOfRestitution;
     }
     
   };
   
   function redraw() {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);  
+    
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     for(var i=0; i < scene.length; i++) {
       scene[i].draw(ctx);
     }
+    
+    // Rotate the canvas so as to oppose the gravity delta
+    canvas.style['WebkitTransform'] = 'rotate('+thisWayUp+'rad)';
+    canvas.style['MozTransform'] = 'rotate('+thisWayUp+'rad)';
+    canvas.style['OTransform'] = 'rotate('+thisWayUp+'rad)';
+    
   };
   
   function simulate() {
     var now = new Date(),
-        interval = now - lastSim;
+        interval = (now - lastSim) / 1000;
+    
+    // Rotate gravity slightly
+    var deltaTheta = rotationFrequency * interval;
+    thisWayDown = (thisWayDown + deltaTheta) % TWO_PI;
+    thisWayUp = (thisWayUp + deltaTheta) % TWO_PI
+    gy = g * Math.cos(thisWayDown);
+    gx = g * Math.sin(thisWayDown);
     
     for(var i=0; i < scene.length; i++) {
       scene[i].advanceSimulationBy(interval);
@@ -97,12 +111,12 @@
   };
   
   var cycleColour = cycle(colours),
-      cycleSize = cycle([3, 5, 7]),
+      cycleSize = cycle(sizes);
   
   function spawn() {
     var p = new Particle(cycleSize());
-    p.dx = (Math.random() -0.5) * 500;
-    p.dy = Math.random() * -400;
+    p.dx = (Math.random() -0.5) * maxInitialComponentVelocity;
+    p.dy = (Math.random() -0.5) * maxInitialComponentVelocity;
     p.fill = cycleColour();
     scene[idx] = p;
     idx = (idx + 1) % maxParticles;
@@ -122,7 +136,7 @@
     canvas.height = canvasHeight;
     
     originX = ~~(canvasWidth * 0.5);
-    originY = ~~(canvasHeight * 0.25);
+    originY = ~~(canvasHeight * 0.5);
     
     ctx = canvas.getContext('2d');
     
